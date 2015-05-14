831c7050aac52ad46c2b23f1deaa6de4f4261402
やこち <jun@yakochi.local>
2015-05-14 17:22:42 +0900

    edit some

--- MouseMove.java
+++ MouseMove.java
@@ -2,8 +2,26 @@
 //import java.awt.*;
 import java.awt.Robot;
 import java.awt.AWTException;
+import javax.swing.JFrame;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.PointerInfo;
+
 public class MouseMove{
 	public static void main(String args[]){
+		// ウィンドウを作成
+		JFrame frame = new JFrame("MouseMove.java");
+		frame.setBounds(300,200,400,350);
+		frame.setVisible(true);
+
+
+		// クリック位置を取得する
+		PointerInfo pI = MouseInfo.getPointerInfo();
+		Point point = pI.getLocation();
+		System.out.println(point.getX()+" "+point.getY());
+
+
+		// マウスポインタの操作
 		Robot r;
 		try { r = new Robot();}
 		catch(AWTException e){
